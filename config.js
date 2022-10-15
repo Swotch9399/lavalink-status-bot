a5469c0442cc3a5c50b6d1b449e4f0a7e93ba00f
Nagıp Taylan G****** <109398760+TaylanGzLn@users.noreply.github.com>
2022-10-15 13:16:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "MTAxMzU2NjYyNTIxNzI0OTM0MA.G7U2Zb.4DKiKJXo85hD4J3uA7Xo0WsrVrMOu6j1SccoXM",  // your bot token
+    token: process.env.TOKEN || "MTAxMzU2NjYyNTIxNzI0OTM0MA.GDy9wo.H71q9f48Zcbx3crqieVynydrd8ExL20iSs5aMs",  // your bot token
     logs: process.env.LOGS || "1013569163110916217", // channel id for lavalink server status logs
    
     nodes: [
