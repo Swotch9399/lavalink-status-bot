93515c6a2e0163dd3fa3a617f2b7919c65d96db0
Nagıp Taylan G****** <109398760+TaylanGzLn@users.noreply.github.com>
2022-10-15 18:16:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "MTAxMzU2NjYyNTIxNzI0OTM0MA.GC-xSd.EJkk_rsBtz5GCh-aG46covX5-DAq3LFa3FXQzg",  // your bot token
+    token: process.env.TOKEN || "MTAxMzU2NjYyNTIxNzI0OTM0MA.G4uW9B.EDIM7vgBPrFvASp8LmtptBqn-CYXu0DpqfdMqc",  // your bot token
     logs: process.env.LOGS || "1013569163110916217", // channel id for lavalink server status logs
    
     nodes: [
         {
-            host: process.env.NODE_HOST || "https://lavalink-node1.herokuapp.com/",
-            identifier: process.env.NODE_ID || "private",
-            port: parseInt(process.env.NODE_PORT || "443"),
-            password: process.env.NODE_PASSWORD || "hawwonode01",
+            host: process.env.NODE_HOST || "lava3.horizxon.studio",
+            identifier: process.env.NODE_ID || "public",
+            port: parseInt(process.env.NODE_PORT || "80"),
+            password: process.env.NODE_PASSWORD || "horizxon.studio",
             secure: parseBoolean(process.env.NODE_SECURE || "false"),
 
         }
