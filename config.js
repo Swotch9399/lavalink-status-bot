7f863b2ef21afd2159c6773c2c3dceb5dd31e3e3
Nagıp Taylan G****** <109398760+TaylanGzLn@users.noreply.github.com>
2022-10-15 13:15:48 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+module.exports = {
+    token: process.env.TOKEN || "MTAxMzU2NjYyNTIxNzI0OTM0MA.G7U2Zb.4DKiKJXo85hD4J3uA7Xo0WsrVrMOu6j1SccoXM",  // your bot token
+    logs: process.env.LOGS || "1013569163110916217", // channel id for lavalink server status logs
+   
+    nodes: [
+        {
+            host: process.env.NODE_HOST || "lava3.horizxon.studio",
+            identifier: process.env.NODE_ID || "public",
+            port: parseInt(process.env.NODE_PORT || "80"),
+            password: process.env.NODE_PASSWORD || "horizxon.studio",
+            secure: parseBoolean(process.env.NODE_SECURE || "false"),
+
+        }
+    ],
+
+}
+
+function parseBoolean(value) {
+    if (typeof (value) === 'string') {
+        value = value.trim().toLowerCase();
+    }
+    switch (value) {
+        case true:
+        case "true":
+            return true;
+        default:
+            return false;
+    }
+}
---
+++ start.bat
@@ -0,0 +1,6 @@
+echo off
+color 6
+cls
+:a
+node index.js
+goto a
