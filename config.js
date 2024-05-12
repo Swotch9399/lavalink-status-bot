9caafa8ab12278d65aab744a195ddf83492301a0
Swotch <109398760+Swotch9399@users.noreply.github.com>
2024-05-12 12:56:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 module.exports = {
     token: process.env.TOKEN || "",  // your bot token
-    logs: process.env.LOGS || "1013569163110916217", // channel id for lavalink server status logs
+    logs: process.env.LOGS || "", // channel id for lavalink server status logs
    
     nodes: [
         {
-            host: process.env.NODE_HOST || "lava3.horizxon.studio",
+            host: process.env.NODE_HOST || "localhost",
             identifier: process.env.NODE_ID || "public",
             port: parseInt(process.env.NODE_PORT || "80"),
-            password: process.env.NODE_PASSWORD || "horizxon.studio",
+            password: process.env.NODE_PASSWORD || "localhost",
             secure: parseBoolean(process.env.NODE_SECURE || "false"),
 
         }
